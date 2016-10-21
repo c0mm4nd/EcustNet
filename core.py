b952a86d1b25a0444dc9c8410ccf97140f0dd318
Command <maoxs2@163.com>
2016-10-21 13:32:53 +0800

    rm run.py & add the Conf

--- core.py
+++ core.py
@@ -2,10 +2,13 @@
 # coding:utf8
 ''' 校园网登录 '''
 ''' http://login.ecust.edu.cn '''
-import requests
-import re
-from lxml import etree
+
+
 def Login(stuID, stuPW):
+    import re
+    import requests
+    from lxml import etree
+
     # valid
     if "http-equiv='refresh'" in requests.get("http://www.baidu.com").text :
         pass
@@ -23,8 +26,6 @@ def Login(stuID, stuPW):
     thirdReq = req.get(thirdUrl)
     finalUrl = thirdReq.url
     html = etree.HTML(thirdReq.text)
-    stuID = 10142045
-    stuPW = 951120
     data = {}
     data["action"] = html.xpath("//input[@name='action']")[0].attrib['value']
     data["ac_id"] = html.xpath("//input[@name='ac_id']")[0].attrib['value']
@@ -42,3 +43,25 @@ def Login(stuID, stuPW):
         return req.cookies #虽然我也不知道这个Cookie有啥用
     else:
         return False
+
+if __name__ == '__main__':
+    import sys
+    import os
+    # print StuID, StuPW
+    if os.path.exists("conf"):
+        # getConf()
+        conf = open("conf","r").readlines()
+        StuID = conf[0]
+        StuPW = conf[1]
+    else :
+        StuID = raw_input("输入学号 Input your StudentID ：")
+        StuPW = raw_input("输入密码 Input your StudentPW ：")
+        conf = open("conf","w+")
+        conf.write(StuID + "\n")
+        conf.write(StuPW + "\n")
+    # print "SID =" + StuID + "SPW =" + StuPW
+    if Login(StuID, StuPW) is False :
+        #返回一个Cookie对象即成功，否则失败。
+        print("失败 False")
+    else ：
+        print("成功 Success")
